291262c8e719fd162eec97097f9b47df04faa8ca
danikiy <danikiy@hotmail.com>
2021-03-18 14:36:57 +0000

    Create index.js

---
+++ tfjs_models/index.js
@@ -0,0 +1,23 @@
+const path_to_model = 'https://github.com/danikiyasseh/danikiyasseh.github.io/tree/master/tfjs_models/model.json'
+// const path_to_model = 'https://drive.google.com/file/d/1yeg8usTgWEW1eR9L8YT2o-ZEVPwZ5lje/view?usp=sharing'
+
+// import * as tf from '@tensorflow/tfjs';
+
+// let net;
+
+async function app() {
+  console.log('Loading mobilenet..');
+
+  // Load the model.
+  // net = await mobilenet.load();
+  const model = await tf.loadLayersModel(path_to_model);
+  console.log('Successfully loaded model');
+
+  // Make a prediction through the model on our image.
+  const imgEl = document.getElementById('img');
+  // const result = await net.classify(imgEl);
+  const result = await model.predict(imgEl)
+  console.log(result);
+}
+
+app();
