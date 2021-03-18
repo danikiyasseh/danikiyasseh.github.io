a3ba7878e8dda0960fd1265ad2470224accc7da8
danikiy <danikiy@hotmail.com>
2021-03-18 15:12:06 +0000

    Update index.js

--- tfjs_models/index.js
+++ tfjs_models/index.js
@@ -9,14 +9,14 @@ async function app() {
   console.log('Loading mobilenet..');
 
   // Load the model.
-  net = await mobilenet.load();
-  // const model = await tf.loadLayersModel(path_to_model);
+  // net = await mobilenet.load();
+  const model = await tf.loadLayersModel(path_to_model);
   console.log('Successfully loaded model');
 
   // Make a prediction through the model on our image.
   const imgEl = document.getElementById('img');
-  const result = await net.classify(imgEl);
-  // const result = await model.predict(imgEl)
+  // const result = await net.classify(imgEl);
+  const result = await model.predict(imgEl)
   console.log(result);
 }
 
