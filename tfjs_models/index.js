e6bbef92c5756c2222b38bdbf047021cb79af60a
danikiy <danikiy@hotmail.com>
2021-03-18 16:04:51 +0000

    Update index.js

--- tfjs_models/index.js
+++ tfjs_models/index.js
@@ -16,7 +16,7 @@ async function app() {
   // Make a prediction through the model on our image.
   const imgEl = document.getElementById('img');
   const img = tf.browser.fromPixels(imgEl);
-  const imgSample = tf.expandDims(img,0);
+  const imgSample = img.reshape([1, 224, 224, 3]);
   // const result = await net.classify(imgEl);
   const result = await model.predict(imgSample);
   console.log(result);
