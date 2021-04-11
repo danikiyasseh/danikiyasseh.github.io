e2d1dea615c42ff4cf662e6ce57c9dfabacb6355
danikiy <danikiy@hotmail.com>
2021-04-11 18:15:41 +0100

    Update audio.js

--- audio/audio.js
+++ audio/audio.js
@@ -1,13 +1,17 @@
-  var wavesurfer = WaveSurfer.create({
-    container: '#waveform'
-    // your options here
-    plugins: [
-        WaveSurfer.spectrogram.create({
-            wavesurfer: wavesurfer,
-            container: "#wave-spectrogram",
-            labels: true
-          })
-      ]
+var wavesurfer = WaveSurfer.create({
+    container: '#waveform',
+    waveColor: '#F2EDD4',
+    progressColor: '#46B54D'
+});
+
+wavesurfer.on('ready', function () {
+  var spectrogram = Object.create(WaveSurfer.Spectrogram);
+  spectrogram.init({
+    wavesurfer: wavesurfer,
+    container: "#wave-spectrogram",
+    fftSamples: 512,
+    labels: true
   });
+});
   
-  wavesurfer.load('./song.mp3');
+wavesurfer.load('./song.mp3');
