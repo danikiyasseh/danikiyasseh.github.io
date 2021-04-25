fc2f6b1dad64a3ae37a140858768e3aab0e28e41
danikiy <danikiy@hotmail.com>
2021-04-25 16:56:13 +0100

    Update index.js

--- audio/index.js
+++ audio/index.js
@@ -64,6 +64,48 @@
 // };
 
 
+class lambdaLayer extends tf.layers.Layer {
+    constructor(config) {
+        super(config);
+        if (config.name === undefined) {
+            config.name = ((+new Date) * Math.random()).toString(36); //random name from timestamp in case name hasn't been set
+        }
+        this.name = config.name;
+        this.lambdaFunction = config.lambdaFunction;
+        this.lambdaOutputShape = config.lambdaOutputShape;
+    }
+
+    call(input) {
+        return tf.tidy(() => {
+            let result = null;
+            eval(this.lambdaFunction);
+            return result;
+        });
+    }
+
+    computeOutputShape(inputShape) {
+        if (this.lambdaOutputShape === undefined) { //if no outputshape provided, try to set as inputshape
+            return inputShape[0];
+        } else {
+            return this.lambdaOutputShape;
+        }
+    }
+
+    getConfig() {
+        const config = super.getConfig();
+        Object.assign(config, {
+            lambdaFunction: this.lambdaFunction,
+   lambdaOutputShape: this.lambdaOutputShape
+        });
+        return config;
+    }
+
+    static get className() {
+        return 'lambdaLayer';
+    }
+}
+tf.serialization.registerClass(lambdaLayer);
+
 
 // Option 2 for recording audio
 
